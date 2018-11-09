0706f9af64080a9c355556285ead6bafe3d134c1
felfsilva <felipefsilva.00@gmail.com>
2018-11-08 23:54:04 -0200

    index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const publicacoes = require('./routes/publicacoes')
 
 const auth = 'PLSgf3yOpjDP8Ap5ysxeEULj4mrT7QbNRQk6zuJ7'
 
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
@@ -14,16 +15,14 @@ app.use(bodyParser.urlencoded())
 app.use('/categorias', categorias)
 app.use('/publicacoes', publicacoes)
 
-const port = process.env.PORT || 3000
 
-// Obter valores do index.ejs
+
+
  app.get('/', async(req, res)=> {
-    // await axios.get('https://como-fazer-63105.firebaseio.com/categorias.json?auth=' + auth)
+     await axios.get('https://como-fazer-63105.firebaseio.com/categorias.json?auth=' + auth)
      res.send('funcionando')
 
  })
-
-// A porta de comunicação do servidor
 app.listen(port, (err) => {
 
     if (err) {console.log('erro na porta')
